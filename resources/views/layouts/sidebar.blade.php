841b39fa0e1aa4b0e9f63bd90fc6d0d9b5ae9f7c
A2blackMZ <samsdinealidou956@gmail.com>
2024-12-14 15:30:11 +0100

    Wekpo First

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,48 @@
+<nav id="sidebar">
+    <div class="sidebar_blog_1">
+       <div class="sidebar-header">
+          <div class="logo_section">
+             <a href="index.html"><img class="logo_icon img-responsive" src="images/logo/logo_icon.png" alt="#" /></a>
+          </div>
+       </div>
+       <div class="sidebar_user_info">
+          <div class="icon_setting"></div>
+          <div class="user_profle_side">
+             <div class="user_img"><img class="img-responsive" src="images/layout_img/user_img.jpg" alt="#" /></div>
+             <div class="user_info">
+                <h6>John David</h6>
+                <p><span class="online_animation"></span> Online</p>
+             </div>
+          </div>
+       </div>
+    </div>
+    <div class="sidebar_blog_2">
+       <h4>General</h4>
+       <ul class="list-unstyled components">
+          <li class="active">
+             <a href="#dashboard" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-dashboard yellow_color"></i> <span>Dashboard</span></a>
+             <ul class="collapse list-unstyled" id="dashboard">
+                <li>
+                   <a href="/">> <span>Default Dashboard</span></a>
+                </li>
+
+             </ul>
+          </li>
+
+          <li class="active">
+             <a href="#additional_page" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-clone yellow_color"></i> <span>Additional Pages</span></a>
+             <ul class="collapse list-unstyled" id="additional_page">
+
+                <li>
+                   <a href="#">> <span>Login</span></a>
+                </li>
+                <li>
+                   <a href="#">> <span>404 Error</span></a>
+                </li>
+             </ul>
+          </li>
+
+          <li><a href="settings.html"><i class="fa fa-cog yellow_color"></i> <span>Settings</span></a></li>
+       </ul>
+    </div>
+ </nav>
